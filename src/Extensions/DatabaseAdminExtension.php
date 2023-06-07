9e3b5dbc756f7469006167f70b6999387cce6746
Florian Thoma <florian.thoma@innoweb.com.au>
2023-06-07 10:30:22 +1000

    fix migration

--- src/Extensions/DatabaseAdminExtension.php
+++ src/Extensions/DatabaseAdminExtension.php
@@ -23,6 +23,7 @@ class DatabaseAdminExtension extends Extension
                     echo "\n<p><b>Migrating Feature Images</b></p>\n\n";
                 }
             }
+
             // find records that need migrating
             $query = "SELECT * from $table WHERE FeatureImageID > 0 AND LocalFeatureImageID = 0";
             $count = DB::query($query)->numRecords();
@@ -32,17 +33,83 @@ class DatabaseAdminExtension extends Extension
                 DB::query($query);
                 if (!$quiet) {
                     if (Director::is_cli()) {
-                        echo sprintf(" * %s images migrated\n", $count);
+                        echo sprintf(" * %s stage images migrated\n", $count);
                     } else {
-                        echo sprintf("<p>%s images migrated</p>\n", $count);
+                        echo sprintf("<p>%s stage images migrated</p>\n", $count);
                     }
                 }
             } else {
                 if (!$quiet) {
                     if (Director::is_cli()) {
-                        echo " * no images needed migration\n";
+                        echo " * no stage images needed migration\n";
                     } else {
-                        echo "<p>No images needed migration</p>\n";
+                        echo "<p>No stage images needed migration</p>\n";
+                    }
+                }
+            }
+            // remove old column
+            $query = "ALTER TABLE $table DROP COLUMN FeatureImageID";
+            DB::query($query);
+        }
+
+        // live
+        $table = Page::config()->get('table_name') . '_Live';
+        $query = "SHOW COLUMNS FROM $table WHERE FIELD = 'FeatureImageID' OR FIELD = 'LocalFeatureImageID'";
+        $count = DB::query($query)->numRecords();
+        if ($count == 2) { // both fields exist
+            // find records that need migrating
+            $query = "SELECT * from $table WHERE FeatureImageID > 0 AND LocalFeatureImageID = 0";
+            $count = DB::query($query)->numRecords();
+            if ($count > 0) {
+                // migrate fields
+                $query = "UPDATE $table set LocalFeatureImageID = FeatureImageID WHERE FeatureImageID > 0 AND LocalFeatureImageID = 0";
+                DB::query($query);
+                if (!$quiet) {
+                    if (Director::is_cli()) {
+                        echo sprintf(" * %s live images migrated\n", $count);
+                    } else {
+                        echo sprintf("<p>%s live images migrated</p>\n", $count);
+                    }
+                }
+            } else {
+                if (!$quiet) {
+                    if (Director::is_cli()) {
+                        echo " * no live images needed migration\n";
+                    } else {
+                        echo "<p>No live images needed migration</p>\n";
+                    }
+                }
+            }
+            // remove old column
+            $query = "ALTER TABLE $table DROP COLUMN FeatureImageID";
+            DB::query($query);
+        }
+
+        // versions
+        $table = Page::config()->get('table_name') . '_Versions';
+        $query = "SHOW COLUMNS FROM $table WHERE FIELD = 'FeatureImageID' OR FIELD = 'LocalFeatureImageID'";
+        $count = DB::query($query)->numRecords();
+        if ($count == 2) { // both fields exist
+            // find records that need migrating
+            $query = "SELECT * from $table WHERE FeatureImageID > 0 AND LocalFeatureImageID = 0";
+            $count = DB::query($query)->numRecords();
+            if ($count > 0) {
+                // migrate fields
+                $query = "UPDATE $table set LocalFeatureImageID = FeatureImageID WHERE FeatureImageID > 0 AND LocalFeatureImageID = 0";
+                DB::query($query);
+                if (!$quiet) {
+                    if (Director::is_cli()) {
+                        echo sprintf(" * %s versions images migrated\n", $count);
+                    } else {
+                        echo sprintf("<p>%s versions images migrated</p>\n", $count);
+                    }
+                }
+            } else {
+                if (!$quiet) {
+                    if (Director::is_cli()) {
+                        echo " * no versions images needed migration\n";
+                    } else {
+                        echo "<p>No versions images needed migration</p>\n";
                     }
                 }
             }
